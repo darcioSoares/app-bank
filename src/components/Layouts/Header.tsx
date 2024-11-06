033df46e87e8473153df3e40388b9f486ccf3662
darcioSoares <darcio80@gmail.com>
2024-11-06 19:14:04 -0300

    deixando header fixo

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo_MINI.png';
 
 const Header = () => {
   return (
-    <header className="w-full bg-gray-50 text-black p-8 h-20 sm:h-24 flex items-center justify-between">
+    <header className="w-full bg-gray-50 text-black p-8 h-15 sm:h-20 flex items-center justify-between fixed top-0 left-0 z-50 shadow-md">
       <img src={logo} alt="Logo" width="300px" />
       <nav className="space-x-4">
         <Link to="/login" className="bg-orange-500 rounded-full py-2 px-6">
